6bc68cf247316a553cd9e2e2006b6a26f06243af
Tóth Kristóf <tothkristof60@gmail.com>
2023-04-08 16:54:48 +0200

    asd

--- GYAK/GYAK06/Node.py
+++ GYAK/GYAK06/Node.py
@@ -1,10 +1,13 @@
 class Node():
-    def __init__(self,feature_index = None,value=None,threshold=None,right=None,info_gain=None) -> None:
-        #leaf
+    def __init__(self, feature_index=None, threshold=None, left=None, right=None, info_gain=None, value=None):
+        ''' constructor ''' 
+        
+        # for decision node
+        self.feature_index = feature_index
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.info_gain = info_gain 
+        
+        # for leaf node
         self.value = value
-
-        #dis
-        self.feature_index=feature_index
-        self.threshold=threshold
-        self.right=right
-        self.info_gain=info_gain
